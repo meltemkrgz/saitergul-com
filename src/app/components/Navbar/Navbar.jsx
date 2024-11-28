24860b6f7584a4613870e917e6ab2f79c3f0a57b
mertkiziltas <mertkizltas@hotmail.com>
2024-11-28 23:52:04 +0300

    navbar oluşturuldu

---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.topbar}>
+        <h1 className={styles.logo}>Logo</h1>
+        <Link href={"#"} className={styles.contactLink}>
+          <img
+            width="24"
+            height="24"
+            src="https://img.icons8.com/ios-filled/50/FFFFFF/phone.png"
+            alt="phone"
+          />
+          İletişim
+        </Link>
+      </div>
+      <ul className={styles.tabbar}>
+        <li>
+            <Link href={"#"}>Anasayfa</Link>
+        </li>
+        <li>
+            <Link href={"#"}>Hizmetlerimiz</Link>
+        </li>
+        <li>
+            <Link href={"#"}>Projeler</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Navbar></Navbar>
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to my website</h1>
       </main>
