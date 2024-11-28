884f05e979107c50929e16605a8fd03dc59d0a3c
meltemkrgz <karagozmeltem@hotmail.com>
2024-11-29 01:28:07 +0300

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,12 +8,22 @@ export default function Home() {
       <Navbar></Navbar>
       <main className={styles.main}>
         <div className={styles.banner}>
-              <h3 className={styles.title}>Hayalinizdeki Ev İçin En Güvenilir Prefabrik Ev Firmasından Teklif Alın</h3>
-              <p>Hayalinizdeki evi, bütçeniz dahilinde inşa edebilecek en tecrübeli ve bilinir prefabrik firmasından ücretsiz teklif alın.</p>
-              <button className={styles.button}>Ücretsiz Teklif Al</button>
-              <Image src={/images/banner.jpg} width={"200"} height={"200"}></Image>
+          <h3 className={styles.title}>
+            Hayalinizdeki Ev İçin En Güvenilir Prefabrik Ev Firmasından Teklif
+            Alın
+          </h3>
+          <p>
+            Hayalinizdeki evi, bütçeniz dahilinde inşa edebilecek en tecrübeli
+            ve bilinir prefabrik firmasından ücretsiz teklif alın.
+          </p>
+          <button className={styles.button}>Ücretsiz Teklif Al</button>
+          <Image
+            src={"/images/banner.jpg"}
+            width={"200"}
+            height={"200"}
+            alt={"banner"}
+          ></Image>
         </div>
-    
       </main>
     </div>
   );
