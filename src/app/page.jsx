e406f9ef539a3c617167911f8b5470b8da01cace
meltemkrgz <karagozmeltem@hotmail.com>
2024-11-28 22:50:03 +0300

    Merge conflict resolved: src/app/page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,7 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-<<<<<<< HEAD:src/app/page.jsx
       <main className={styles.main}>aşkım</main>
-=======
-      mert modified
->>>>>>> f17af3a86cee7b701da252e6da8e927f15b452e7:src/app/page.js
     </div>
   );
 }
