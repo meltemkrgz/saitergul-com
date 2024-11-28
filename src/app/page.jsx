9b26caf2b5d357cb790a8dbadf38d24fbcda0452
meltemkrgz <karagozmeltem@hotmail.com>
2024-11-28 22:40:46 +0300

    page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-
+      <main className={styles.main}>aşkım</main>
     </div>
   );
 }
